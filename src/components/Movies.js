cf5f362f408161ad66137c334e8359b39f6b3bbe
Muzaffar <muzaffarsafarov880@gmail.com>
2022-09-11 21:49:16 +0500

    Debugging and not found page

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Movie from './Movie';
+import Movie from "./Movie";
 
-function Movies(props) {
-    const {movies} = props;
-    return (
-        <div className='movies'>
-            {movies.map(movie => (
-                <Movie key={movie.imdbID} {...movie} />
-            ))}
-        </div>
-    );
+export default function Movies(props) {
+  const { movies = [] } = props;
+
+  return (
+    <div className="movies">
+      {movies.length ? (
+        movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
+      ) : (
+        <h4>Nothing found</h4>
+      )}
+    </div>
+  );
 }
-
-export default Movies;
